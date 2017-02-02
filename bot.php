760aa50c8968a6bb86ec50d3405f25178622ced5
warote001 <warot_note@hotmail.com>
2017-02-02 19:59:17 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -24,7 +24,7 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
-			// Build message to reply back
+			// Build message to reply back 
 			$messages = [
 				'type' => 'text',
 				'text' => $sendtext,
