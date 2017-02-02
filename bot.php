a87d256ea2a8961977206dee9ff1705c2646848c
warote001 <warot_note@hotmail.com>
2017-02-02 18:14:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -39,7 +39,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $sendtext
+				'text' => $sendtext,
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
