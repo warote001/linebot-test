f6a59f1503515932c087fc65adbd4d2b49bafe8a
warote001 <warot_note@hotmail.com>
2017-02-03 10:08:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -59,43 +59,42 @@ if (!is_null($events['events'])) {
 			}else if(strtoupper($text) == "ZOMBIE"){
 				$sendtext = "และวันนี้เขาจะฝุกฝังในความทรงจำ https://www.youtube.com/watch?v=B6h5iJJtevo";
 // 				respondtext($sendtext,$replytoken);
-				echo $result . "\r\n";
 			}else if($text == "ความเชื่อ"){
 				$sendtext = "ที่ผ่านมา หมูป่าชนะมาได้ก็เป็นเพียงแค่โชคช่วย https://www.youtube.com/watch?v=aKYdF61sO2c";
 // 				respondtext($sendtext,$replytoken);
-				echo $result . "\r\n";
 			}else if(strtoupper($text) == "JOHN"){
 				$sendtext = "นี่เพื่อนผม JOHN ที่ตอบได้แต่ค้าบ";
 // 				respondimage($sendimg,$replytoken);
 // 				respondtext($sendtext,$replytoken);
-				echo $result . "\r\n";
 			}else if($text == "อีกาดำ ถอดหน้ากาก"){
 				$room = $event['source'];
 // 				respondtext($room,$replytoken);
-				echo $result . "\r\n";
 			}
 			// Build message to reply back
-	$messages = [
-			'type' => 'text',
-			'text' => $sendtext,
-	];
-	// Make a POST Request to Messaging API to reply to sender
-	$url = 'https://api.line.me/v2/bot/message/reply';
-	$data = [
-			'replyToken' => $replyToken,
-			'messages' => [$messages],
-	];
-	$post = json_encode($data);
-	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	$result = curl_exec($ch);
-	curl_close($ch);
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
 		}
 	}
 }
