108a142e2d26195e763a21641fcc103382110bb0
warote001 <warot_note@hotmail.com>
2017-02-03 08:56:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,46 +10,7 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			if($text == "อีกาดำ"){
-				$sendtext = "คนที่เหนือกว่าเขาทุกอย่าง คือ ผม อีกาดำ";
-			}else if(strtoupper($text) == "zombie"){
-				$sendtext = "แล้ววันนี้ เขาจะถูกฝั่งอยู่ในความทรงจำ https://www.youtube.com/watch?v=B6h5iJJtevo";
-			}else if($text == "อีกาดำสุดยอด"){
-				$sendtext = "การแข่งขันยอมมีแพ้ชนะเสมอ https://www.youtube.com/watch?v=dwlJbb0o8V8";
-			}
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back 
-			$messages = [
-				'type' => 'text',
-				'text' => $sendtext,
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
+		
 	}
 }
 echo "OK";
