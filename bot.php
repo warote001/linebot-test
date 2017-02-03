dba7623c15b93f816e79e1c32cf37f71e1df88ed
warote001 <warot_note@hotmail.com>
2017-02-03 10:04:56 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,28 +1,28 @@
 
 <?php
 function respondtext($sendtext,$replyToken){
-	// Build message to reply back
-	$messages = [
-			'type' => 'text',
-			'text' => $sendtext,
-	];
-	// Make a POST Request to Messaging API to reply to sender
-	$url = 'https://api.line.me/v2/bot/message/reply';
-	$data = [
-			'replyToken' => $replyToken,
-			'messages' => [$messages],
-	];
-	$post = json_encode($data);
-	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+// 	// Build message to reply back
+// 	$messages = [
+// 			'type' => 'text',
+// 			'text' => $sendtext,
+// 	];
+// 	// Make a POST Request to Messaging API to reply to sender
+// 	$url = 'https://api.line.me/v2/bot/message/reply';
+// 	$data = [
+// 			'replyToken' => $replyToken,
+// 			'messages' => [$messages],
+// 	];
+// 	$post = json_encode($data);
+// 	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	$result = curl_exec($ch);
-	curl_close($ch);
+// 	$ch = curl_init($url);
+// 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// 	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+// 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+// 	$result = curl_exec($ch);
+// 	curl_close($ch);
 }
 function leavechat(){
 	// Make a POST Request to Messaging API to reply to sender
@@ -58,22 +58,44 @@ if (!is_null($events['events'])) {
 				$sendtext = "คนที่เหนือกว่าเขาทุกอย่างคือ ผม อีกาดำ";
 			}else if(strtoupper($text) == "ZOMBIE"){
 				$sendtext = "และวันนี้เขาจะฝุกฝังในความทรงจำ https://www.youtube.com/watch?v=B6h5iJJtevo";
-				respondtext($sendtext,$replytoken);
+// 				respondtext($sendtext,$replytoken);
 				echo $result . "\r\n";
 			}else if($text == "ความเชื่อ"){
 				$sendtext = "ที่ผ่านมา หมูป่าชนะมาได้ก็เป็นเพียงแค่โชคช่วย https://www.youtube.com/watch?v=aKYdF61sO2c";
-				respondtext($sendtext,$replytoken);
+// 				respondtext($sendtext,$replytoken);
 				echo $result . "\r\n";
 			}else if(strtoupper($text) == "JOHN"){
 				$sendtext = "นี่เพื่อนผม JOHN ที่ตอบได้แต่ค้าบ";
-				respondimage($sendimg,$replytoken);
-				respondtext($sendtext,$replytoken);
+// 				respondimage($sendimg,$replytoken);
+// 				respondtext($sendtext,$replytoken);
 				echo $result . "\r\n";
 			}else if($text == "อีกาดำ ถอดหน้ากาก"){
 				$room = $event['source'];
-				respondtext($room,$replytoken);
+// 				respondtext($room,$replytoken);
 				echo $result . "\r\n";
 			}
+			// Build message to reply back
+	$messages = [
+			'type' => 'text',
+			'text' => $sendtext,
+	];
+	// Make a POST Request to Messaging API to reply to sender
+	$url = 'https://api.line.me/v2/bot/message/reply';
+	$data = [
+			'replyToken' => $replyToken,
+			'messages' => [$messages],
+	];
+	$post = json_encode($data);
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
 		}
 	}
 }
