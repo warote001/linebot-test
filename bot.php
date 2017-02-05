26b8d911bd46de0bb3e71a730aaf4a024f00ea53
warote001 <warot_note@hotmail.com>
2017-02-05 16:47:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,7 +9,7 @@ function replytext($text,$event){
 		$sendtext = "ที่ผ่านมา หมูป่าชนะมาได้ก็เป็นเพียงแค่โชคช่วย https://www.youtube.com/watch?v=aKYdF61sO2c";
 	}else if(strtoupper($text) == "JOHN"){
 		$sendtext = "นี่เพื่อนผม JOHN ที่ตอบได้แต่ค้าบ";
-	}else if($text == "อีกาดำ ถอดหน้ากาก"){
+	}else if($text == "อีกาดำ ถอดหน้ากากครับ"){
 		$sendtext = "ลาก่อย";
 	}else{
 		$sendtext = "001";
